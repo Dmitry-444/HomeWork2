c8191d4ddaccb8668f012b47344410e22f9a7eab
Dmitry-444 <piervov00@gmail.com>
2021-03-22 21:53:32 +0300

    HW3_2_Update

--- app/src/main/java/com/example/homework2/MainActivity.kt
+++ app/src/main/java/com/example/homework2/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.homework2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.signin_layout)
-    }
+        setContentView(R.layout.activity_main)
 
+        val firstFragment = FirstFragment()
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, firstFragment)
+        transaction.commit()
+
+    }
 }
